609f1b36a642258c2c5b6076671e855c5cd95fdb
AadiP9 <parashararyaman18@gmail.com>
2025-09-20 14:15:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ app.use('/api/misc', require('./routes/miscRoutes'));
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
+});
